225fbd911ad40b8ae74073cad952bcfc3efd4cb6
Karman Singh <t.karman.singh@gmail.com>
2026-07-25 22:43:34 +0530

    style: change background color to greyish black

--- src/components/notebook/NotebookController.tsx
+++ src/components/notebook/NotebookController.tsx
@@ -33,7 +33,7 @@ export function NotebookController() {
   return (
     <div 
       ref={containerRef}
-      className="w-full relative bg-[#121212]"
+      className="w-full relative bg-[#2a2a2a]"
       style={{ height: `${totalPages * 150}vh` }}
     >
       {/* Inverted Crumpled Texture for white creases on black paper */}
